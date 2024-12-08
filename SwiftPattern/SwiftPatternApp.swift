de0a8408e1b2df2f5a88e32833456d026e268c36
San <santhoshi2005@gmail.com>
2024-12-07 19:09:18 -0800

    Initial Commit

---
+++ SwiftPattern/SwiftPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPatternApp.swift
+//  SwiftPattern
+//
+//  Created by Santhoshi Guduru on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
